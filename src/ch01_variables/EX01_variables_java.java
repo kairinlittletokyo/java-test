2ba83bbf4b38f80c0d4757d4db71001f3582ec8d
kairinlittletokyo <dbflrodutls123@gmail.com>
2023-11-30 13:28:00 +0900

    TOP SECRET

---
+++ src/ch01_variables/EX01_variables_java.java
@@ -0,0 +1,20 @@
+package ch01_variables;
+
+public class EX01_variables_java {
+
+	public static void main(String[] args) {
+		//본인의 이름(name), 나이(age), 키, (height) 주소(address), 
+		//전화번호(mobile)를 입력할 수 있는 변수를 각각 선언
+		
+		String name = "최지웅";
+		int age = 24;
+		int height = 178;
+		String address = "마계 인천"; 
+		String Mobile = "010-666-4444";
+		
+	}
+}
+
+
+
+
